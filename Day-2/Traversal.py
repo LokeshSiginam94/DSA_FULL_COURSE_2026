349d5fdf73784afd81f1fad5761d7e8568e993ec
lokeshsiginam94 <lokeshsiginam199@gmail.com>
2026-09-13 21:44:40 +0530

    Add Traversal

---
+++ Day-2/Traversal.py
@@ -0,0 +1,7 @@
+numbers= [10,20,30,40,50]
+
+for x in numbers:
+    print(x)
+
+#time complexity of this code is O(n) where n is the number of elements in the list. This is because we are iterating through each element in the list once.
+

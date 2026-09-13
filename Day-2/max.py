f14a589ed2868e7316b205ca29a561d8d546493c
lokeshsiginam94 <lokeshsiginam199@gmail.com>
2026-09-13 22:17:42 +0530

    Add Max/Min

---
+++ Day-2/max.py
@@ -0,0 +1,8 @@
+nums = [15, 7, 42, 23, 91, 36]
+
+max=15
+
+for i in nums:
+    if i>max:
+        max=i
+print("The maximum number in the list is:", max)        
---
+++ Day-2/min.py
@@ -0,0 +1,7 @@
+nums=[1,56,22,78,90,34]
+min=1
+
+for i in nums:
+    if i<min:
+        min=i
+print("The minimum number in the list is:", min)

ca79899825a2285a2471dfbc69fd2f007eb88ab3
lokeshsiginam94 <lokeshsiginam199@gmail.com>
2026-09-13 16:35:26 +0530

    Add Arrays,T&S Complexity

---
+++ Day-1/Array.py
@@ -0,0 +1,12 @@
+students=["lokesh","suresh","rajesh","mahesh"]
+
+for student in students:
+    print(student)
+
+for student in students:
+    if student=="lokesh":
+        print("present")
+    else:
+        print("absent")
+
+print(len(students))
---
+++ Day-1/Time_complexity.py
@@ -0,0 +1,9 @@
+numbers = [10,20,30,40,50]
+print(numbers[3]) #40
+
+NUMBERS=[10,20,30,40,50]
+for x in NUMBERS:
+    print(x) #10,20,30,40,50
+
+#0(n)
+#50times
